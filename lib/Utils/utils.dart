58c01d287de22528dcbb573643a05d0e88725fa5
avadhkatrodiya3003 <avadhworkzone@gmail.com>
2024-01-07 13:39:03 +0530

    init commit

---
+++ lib/Utils/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils {
+  static const whiteColor = Colors.white;
+  static const redColor = Colors.red;
+  static const blackColor = Colors.black;
+  static const blueColor = Colors.blue;
+  static const greyColor = Colors.grey;
+  static const greenColor = Color(0xff54D27F);
+}
---
+++ lib/view/loginScreen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
